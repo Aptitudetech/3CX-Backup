601865caa9ee522af77e4be74ea5848f91137061
Bernard Lefebvre <bernard.lefebvre@aptitudetech.net>
2017-02-03 23:41:07 -0500

    Update backup_to_s3.sh

--- backup_to_s3.sh
+++ backup_to_s3.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 # Upload files to AWS S3 bucket using curl
 
-S3_KEY=
-S3_SECRET=
-S3_BUCKET=bkup-3cx-aptech
-S3_PATH=
+HMAC-SHA256s(){
+ KEY="$1"
+ DATA="$2"
+ shift 2
+ printf "$DATA" | openssl dgst -binary -sha256 -hmac "$KEY" | od -An -vtx1 | sed 's/[ \n]//g' | sed 'N;s/\n//'
+}
 
-FILES="/var/lib/3cxpbx/Instance1/Data/Backups"
+HMAC-SHA256h(){
+ KEY="$1"
+ DATA="$2"
+ shift 2
+ printf "$DATA" | openssl dgst -binary -sha256 -mac HMAC -macopt "hexkey:$KEY" | od -An -vtx1 | sed 's/[ \n]//g' | sed 'N;s/\n//'
+}
 
-for file in $FILES; do
-  echo -n "Transferring file $file..."
 
-  export LC_ALL=C
-  DATE=$(date +"%a, %d %b %Y %T %z")
-  ACL="x-amz-acl:private"
-  CONTENT_TYPE=$(file -b --mime-type $file)
-  STRING="PUT\n\n$CONTENT_TYPE\n$DATE\n$ACL\n/$S3_BUCKET$S3_PATH$file"
-  SIGNATURE=$(echo -en "${STRING}" | openssl sha1 -hmac "${S3_SECRET}" -binary | base64)
+AWS_SECRET_KEY=
+AWS_ACCESS_KEY=
 
-  curl -X PUT -T "$file" \
-    -H "Host: $S3_BUCKET.s3.amazonaws.com" \
-    -H "Date: $DATE" \
-    -H "Content-Type: $CONTENT_TYPE" \
-    -H "$ACL" \
-    -H "Authorization: AWS ${S3_KEY}:$SIGNATURE" \
-    "https://$S3_BUCKET.s3.amazonaws.com$S3_PATH$file"
+FILE_TO_UPLOAD="/var/lib/3cxpbx/Instance1/Data/Backups/3CXScheduledBackup.zip"
+BUCKET="bucket"
+STARTS_WITH="domaine.tld/3CXBackup.zip"
 
-  echo "done."
-done
+REQUEST_TIME=$(date +"%Y%m%dT%H%M%SZ")
+REQUEST_REGION="ca-central-1"
+REQUEST_SERVICE="s3"
+REQUEST_DATE=$(printf "${REQUEST_TIME}" | cut -c 1-8)
+AWS4SECRET="AWS4"$AWS_SECRET_KEY
+ALGORITHM="AWS4-HMAC-SHA256"
+EXPIRE="2017-03-03T00:00:00.000Z"
+ACL="private"
+
+POST_POLICY='{"expiration":"'$EXPIRE'","conditions": [{"bucket":"'$BUCKET'" },{"acl":"'$ACL'" },["starts-with", "$key", "'$STARTS_WITH'"],["eq", "$Content-Type", "application/octet-stream"],{"x-amz-credential":"'$AWS_ACCESS_KEY'/'$REQUEST_DATE'/'$REQUEST_REGION'/'$REQUEST_SERVICE'/aws4_request"},{"x-amz-algorithm":"'$ALGORITHM'"},{"x-amz-date":"'$REQUEST_TIME'"}]}'
+
+UPLOAD_REQUEST=$(printf "$POST_POLICY" | openssl base64 )
+UPLOAD_REQUEST=$(echo -en $UPLOAD_REQUEST |  sed "s/ //g")
+
+SIGNATURE=$(HMAC-SHA256h $(HMAC-SHA256h $(HMAC-SHA256h $(HMAC-SHA256h $(HMAC-SHA256s $AWS4SECRET $REQUEST_DATE ) $REQUEST_REGION) $REQUEST_SERVICE) "aws4_request") $UPLOAD_REQUEST)
+
+curl --silent \
+	-F "key=""$STARTS_WITH" \
+	-F "acl="$ACL"" \
+	-F "Content-Type="application/octet-stream"" \
+	-F "x-amz-algorithm="$ALGORITHM"" \
+	-F "x-amz-credential="$AWS_ACCESS_KEY/$REQUEST_DATE/$REQUEST_REGION/$REQUEST_SERVICE/aws4_request"" \
+	-F "x-amz-date="$REQUEST_TIME"" \
+	-F "Policy="$UPLOAD_REQUEST"" \
+	-F "X-Amz-Signature="$SIGNATURE"" \
+	-F "file=@"$FILE_TO_UPLOAD https://$BUCKET.s3.$REQUEST_REGION.amazonaws.com/
